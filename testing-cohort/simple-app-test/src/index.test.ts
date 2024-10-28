0009ebe019cf287d33fbde9cc192bb31d510b974
ambitious_hustler <deepaks9631342845@gmail.com>
2024-10-28 12:49:15 +0530

    finish off with testing

--- testing-cohort/simple-app-test/src/index.test.ts
+++ testing-cohort/simple-app-test/src/index.test.ts
@@ -6,4 +6,12 @@ describe ('sum module', ()=>{
   test('adds 1+2 is equal to 3', ()=>{
     expect(sum(1,2)).toBe(3)
   });
+
+  test("add 2+5 is equal to 7", ()=>{
+    expect(sum(2,5)).toBe(7)
+  });
+
+  test("sum 5+5 is equal to 10", ()=>{
+    expect(sum(5,5)).toBe(10)
+  })
 })
